7d4b7f4d5c7f90904e76e4065e5bad7744a8b9e1
thientdk <duckhanhthientran@gmail.com>
2025-08-19 14:49:19 +0700

    commit changes

--- src/main/java/com/thientdk/be_auth/controllers/AuthController.java
+++ src/main/java/com/thientdk/be_auth/controllers/AuthController.java
@@ -3,11 +3,9 @@ package com.thientdk.be_auth.controllers;
 import com.thientdk.be_auth.models.requests.LoginRequest;
 import com.thientdk.be_auth.models.requests.SignupRequest;
 import com.thientdk.be_auth.models.responses.LoginResponse;
-import com.thientdk.be_auth.models.responses.TextResponse;
 import com.thientdk.be_auth.models.responses.UserResponse;
 import com.thientdk.be_auth.services.AuthService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -31,4 +29,6 @@ public class AuthController {
     }
 
     //todo: refresh token and introspect
+    //todo: add swagger to the app
+    //todo: save log login attempts
 }
--- src/main/java/com/thientdk/be_auth/controllers/UserController.java
+++ src/main/java/com/thientdk/be_auth/controllers/UserController.java
@@ -3,7 +3,6 @@ package com.thientdk.be_auth.controllers;
 import com.thientdk.be_auth.models.responses.UserResponse;
 import com.thientdk.be_auth.services.UserService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
